b30316dd81125594521aee6d4e07d530a25348fb
ANURAG VERMA <anurag.verma54@gmail.com>
2026-08-21 23:53:02 +0530

    Palindrome Number

--- src/mathsProgram/palindromeNumbers.java
+++ src/mathsProgram/palindromeNumbers.java
@@ -1,5 +1,29 @@
 package mathsProgram;
 
 public class palindromeNumbers {
+	
+	public static boolean isPalindrome(int x) {
+		if (x < 0) {
+			return false;
+		}
+		int original = x;
+		int reversed = 0;
+		while (x != 0) {
+			int digit = x % 10;
+			reversed = reversed * 10 + digit;
+			x /= 10;
+		}
+		return original == reversed;
+	}
+	
+	public static void main(String[] args) {
+		int num1 = 121;
+		int num2 = -121;
+		int num3 = 10;
+		
+		System.out.println(num1 + " is palindrome: " + isPalindrome(num1));
+		System.out.println(num2 + " is palindrome: " + isPalindrome(num2));
+		System.out.println(num3 + " is palindrome: " + isPalindrome(num3));
+	}
 
 }
